d374924b7c9673ac82a9b66eb5398cf869c6b188
asim1306 <142720038+asim1306@users.noreply.github.com>
2024-05-01 21:42:01 +0530

    Add files via upload
    This is our Final project on topic Fitness Tracker.
    Here are the group members name :-
    Asim Kalim (IIT2023219)
    Bikram Barman (IIT2023233)
    Piyush Chauhan (IIT2023221)
    Navanth Sai (IIT202329)

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+app.use(require("./routes/apiRoutes.js"));
+require("./routes/htmlRoutes")(app);
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
